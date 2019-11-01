807c6197d4fda94b22a3c7896c0f2bb7fe1c324c
MissBolin <36969668+MissBolin@users.noreply.github.com>
2019-11-01 10:28:52 -0700

    Prints Comments Data Types

---
+++ notes.py
@@ -0,0 +1,28 @@
+# My Class Notes
+
+# Printing Strings
+print("hello world")
+print("This is a string")
+print("123 abc .,")
+print('check this')
+# You can use apostrophes or quotes
+
+# Printing Numbers
+# Integers have no decimal place
+print(7)
+print(-10)
+# floating point numbers HAVE a decimal place
+print(3.14)
+print(-10.)
+
+# Printing Booleans
+print(True)
+print(False)
+
+# This is a comment
+"""
+This is a multi -
+line comment and I 
+can keep writing
+on multiple lines
+"""
