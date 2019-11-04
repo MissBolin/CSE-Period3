fd535b8fc0c2d703725e1a4d1c055f1814dfcfbb
MissBolin <36969668+MissBolin@users.noreply.github.com>
2019-11-04 10:54:41 -0800

    Math

--- notes.py
+++ notes.py
@@ -27,4 +27,65 @@ can keep writing
 on multiple lines
 """
 
+# Basic Math
+print(8+3)
+print(8-3)
+print(8*3)  # shift + 8
+print(8/3)
 
+# Semi-advanced Math
+
+# Exponents or Powers
+print(2**3)  # 8
+print(1**7)  # 1
+
+# Floor Division
+print(11//5)
+print(51//10)
+
+# Remainder After Division
+# Also called 'MOD'
+print(51 % 10)
+print(77 % 25)
+
+# Variables
+# Store Data
+
+my_number = 7
+print(my_number)
+
+name = "John Doe"
+print("Welcome")
+print(name)
+
+food = False
+print(food)
+
+# Update variables
+cost = 12
+cost += 4  # Add 4
+print(cost)
+
+
+price = 10
+price -= 3  # subtract 3
+print(price)
+
+price = 10
+price *= 3  # Multiply 3
+print(price)
+
+price = 10
+price /= 3  # Divide by 3. Return the float (decimal)
+print(price)
+
+# Writing a program
+small = 12
+middle = 15
+large = 20
+small = large - middle
+large = large % small
+middle = large + small
+print(small)
+print(middle)
+print(large)
